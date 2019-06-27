8ce7a0b67dc189c567f3ea7a102bc9cf4d33b439
katraaa <katrennadalouhichi@gmail.com>
2019-06-27 12:42:20 +0100

    second commit

--- scratch.py
+++ scratch.py
@@ -1,2 +1,7 @@
-print ('hello world') \
+import urllib.request
+import urllib
+import json
 
+response = urllib.request.urlopen('https://api.monarchinitiative.org/api/swagger.json')
+print('RESPONSE:', response)
+print('URL     :', response.geturl())
