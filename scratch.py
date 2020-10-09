0a31024b1be49487a44aecca1d402e881ed53f8d
Katr Ennada <51276056+katraaa@users.noreply.github.com>
2020-10-09 05:38:34 +0100

    Delete scratch.py

--- scratch.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import urllib
-import json
-
-response = urllib.request.urlopen('https://api.monarchinitiative.org/api/swagger.json')
-print('RESPONSE:', response)
-print('URL     :', response.geturl())
